faf91a4e904075d0dc6b0113b973b8cfe2567007
varsha0201 <varshamohite0201@gmail.com>
2020-03-06 16:42:46 +0530

    intersting_stuff_in_python

---
+++ python_audio_recording.py
@@ -0,0 +1,12 @@
+# pip3 install sounddevice
+# pip3 install scipy
+
+import sounddevice as sound_device
+from scipy.io.wavfile import write as audio_write
+
+fs=44100
+seconds=15
+
+recording=sound_device.rec(int(seconds*fs),samplerate=fs,channels=2)
+sound_device.wait()
+audio_write("recording.wav",fs,recording)
